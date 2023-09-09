2998026025bc85e1c01bf6286f66796c90a608c1
Ignacio de Miguel Díaz <imigueldiaz@gmail.com>
2023-09-09 17:39:44 +0200

    Some tests are passing now

--- src/cameras_db/controllers.py
+++ src/cameras_db/controllers.py
@@ -16,77 +16,36 @@ class CamerasController:
         self.conn = sqlite3.connect(db_path)
         self.cursor = cursor if cursor is not None else self.conn.cursor()
 
-    def row_to_camera(self, row) -> Camera:
-        """
-        Converts a row from the database into a Camera object.
-
-        Parameters:
-            row (tuple): A tuple representing a row from the database.
-
-        Returns:
-            Camera: An instance of the Camera class.
-
-        Raises:
-            None
-        """
-        columns = [desc[0] for desc in self.cursor.description]
+    def row_to_camera(self, row, columns) -> Camera:
         row_dict = dict(zip(columns, row))
+        print("Row dict:", row_dict)
         return Camera(**row_dict)
 
     def get_by_field(self, field: str, value: Union[str, int, float]) -> List[Camera]:
-        """
-        Retrieves a list of cameras from the database based on a specific field and value.
-
-        Args:
-            field (str): The field to search for in the database.
-            value (Union[str, int, float]): The value to match in the specified field.
-
-        Returns:
-            List[Camera]: A list of Camera objects that match the given field and value.
-             If no matching cameras are found, an empty list is returned.
-        """
-
         query = f"SELECT * FROM cameras WHERE {field} = ?"
         self.cursor.execute(
             query,
-            (
-                field,
-                value,
-            ),
+            (value,),
         )
         rows = self.cursor.fetchall()
-        cameras = [self.row_to_camera(row) for row in rows]
+        columns = [desc[0] for desc in self.cursor.description]
+        cameras = [self.row_to_camera(row, columns) for row in rows]
         return cameras if cameras else []
 
     def get_by_field_like(self, field: str, value: str) -> List[Camera]:
-        """
-        Get a list of cameras from the database that match a given field and value.
-
-        Args:
-            field (str): The field to search in the database.
-            value (str): The value to search for in the specified field.
-
-        Returns:
-            List[Camera]: A list of Camera objects that match the specified field and value.
-            If no matches are found, an empty list is returned.
-        """
-        query = "SELECT * FROM cameras WHERE {} LIKE ?".format(field)
-        param = "%{}%".format(value)
-        self.cursor.execute(query, (param,))
+        query = f"SELECT * FROM cameras WHERE {field} LIKE ?"
+        self.cursor.execute(
+            query,
+            (
+                "%{}%".format(value),
+            ),  # Pass only one value with the % wildcard characters
+        )
         rows = self.cursor.fetchall()
-        return [self.row_to_camera(row) for row in rows] if rows else []
+        columns = [desc[0] for desc in self.cursor.description]
+        cameras = [self.row_to_camera(row, columns) for row in rows]
+        return cameras if cameras else []
 
     def get_by_fields_like_and(self, field_value_dict: Dict[str, str]) -> List[Camera]:
-        """
-        Retrieves a list of Camera objects from the database based on the provided field-value dictionary.
-
-        Args:
-            field_value_dict (Dict[str, str]): A dictionary mapping field names to their corresponding values.
-
-        Returns:
-            List[Camera]: A list of Camera objects that match the specified field-value criteria.
-            If no matches are found, an empty list is returned.
-        """
         query_fields = [
             f"{field} LIKE ? COLLATE NOCASE" for field in field_value_dict.keys()
         ]
@@ -95,22 +54,11 @@ class CamerasController:
 
         self.cursor.execute(query, params)
         rows = self.cursor.fetchall()
+        columns = [column[0] for column in self.cursor.description]
 
-        return [self.row_to_camera(row) for row in rows] if rows else []
+        return [self.row_to_camera(row, columns) for row in rows] if rows else []
 
     def get_by_fields_like_or(self, field_value_dict: Dict[str, str]) -> List[Camera]:
-        """
-        Retrieves a list of Camera objects from the database that match the specified fields
-        and values in a case-insensitive manner.
-
-        Args:
-            field_value_dict (Dict[str, str]): A dictionary containing the field names as keys
-            and the corresponding values to match as values.
-
-        Returns:
-            List[Camera]: A list of Camera objects that match the specified fields and values.
-            Returns an empty list if no matches are found.
-        """
         query_fields = [
             f"{field} LIKE ? COLLATE NOCASE" for field in field_value_dict.keys()
         ]
@@ -119,8 +67,9 @@ class CamerasController:
 
         self.cursor.execute(query, params)
         rows = self.cursor.fetchall()
+        columns = [column[0] for column in self.cursor.description]
 
-        return [self.row_to_camera(row) for row in rows] if rows else []
+        return [self.row_to_camera(row, columns) for row in rows] if rows else []
 
     def get_by_fields_with_operators(
         self, field_value_dict: Dict[str, str], operators: Dict[str, str]
--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,6 +1,4 @@
-from typing import List, Tuple
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
 
 from cameras_db.controllers import CamerasController
 from cameras_db.constants import CREATE_CAMERA_TABLE_QUERY
@@ -10,43 +8,32 @@ class TestCamerasController(TestCase):
     from unittest.mock import patch
 
     def setUp(self):
-        mock_cursor = MagicMock()
+        # Create an in-memory SQLite3 database
+        self.controller = CamerasController(":memory:")
 
-        mock_description = [
-            (key,)
-            for key in [
-                "brand",
-                "model",
-                "sensor_size_w",
-                "sensor_size_h",
-                "sensor_px_w",
-                "sensor_px_h",
-            ]
+        # Create the cameras table
+        self.controller.cursor.execute(CREATE_CAMERA_TABLE_QUERY)
+
+        # Insert mock data into the cameras table
+        mock_data = [
+            ("Canon", "EOS R6", 36, 24, 6720, 4480),
+            ("Nikon", "D750", 36, 24, 6720, 4480),
+            ("Sony", "A7 III", 35.6, 23.8, 6000, 4000),
         ]
-        mock_cursor.configure_mock(description=mock_description)
 
-        # Mock the execute and fetchall/one methods if needed
-        mock_cursor.execute.return_value = None
-        mock_cursor.fetchall.return_value = []  # Or mock data you expect to fetch
+        self.controller.cursor.executemany(
+            """
+            INSERT INTO cameras (brand, model, sensor_size_w, sensor_size_h, sensor_px_w, sensor_px_h)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            mock_data,
+        )
 
-        self.controller = CamerasController(":memory:", cursor=mock_cursor)
-
-        # Initialize the database schema
-        mock_cursor.execute(CREATE_CAMERA_TABLE_QUERY)
+        # Commit the changes
+        self.controller.conn.commit()
 
     def test_row_to_camera(self):
-        mock_description: list[tuple[str]] = [
-            (key,)
-            for key in [
-                "brand",
-                "model",
-                "sensor_size_w",
-                "sensor_size_h",
-                "sensor_px_w",
-                "sensor_px_h",
-            ]
-        ]
-
+        # Insert a test row into the database
         mock_row = (
             "Canon",
             "EOS 5D",
@@ -55,9 +42,28 @@ class TestCamerasController(TestCase):
             6720,
             4480,
         )
+        self.controller.cursor.execute(
+            """
+                INSERT INTO cameras (brand, model, sensor_size_w, sensor_size_h, sensor_px_w, sensor_px_h)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """,
+            mock_row,
+        )
+        self.controller.conn.commit()
 
-        camera = self.controller.row_to_camera(mock_row)
+        # Fetch the test row from the database
+        self.controller.cursor.execute(
+            "SELECT * FROM cameras WHERE brand = 'Canon' AND model = 'EOS 5D'"
+        )
+        fetched_row = self.controller.cursor.fetchone()
 
+        # Get the column names from the cursor's description
+        columns = [desc[0] for desc in self.controller.cursor.description]
+
+        # Call the row_to_camera method with the fetched row and columns
+        camera = self.controller.row_to_camera(fetched_row, columns)
+
+        # Assert the camera object has the correct attributes
         self.assertEqual(camera.brand, "Canon")
         self.assertEqual(camera.model, "EOS 5D")
         self.assertEqual(camera.sensor_size_w, 36.0)
@@ -66,57 +72,44 @@ class TestCamerasController(TestCase):
         self.assertEqual(camera.sensor_px_h, 4480)
 
     def test_get_by_field(self):
-        # Arrange
-
-        def fetchall_side_effect(*args, **kwargs):
-            query, params = args
-            field, value = params
-            return [row for row in mock_data if row[field] == value]
-
-        mock_data = [
-            {
-                "brand": "Canon",
-                "model": "EOS R6",
-                "sensor_size_w": 36,
-                "sensor_size_h": 24,
-                "sensor_px_w": 6720,
-                "sensor_px_h": 4480,
-            },
-            {
-                "brand": "Nikon",
-                "model": "D750",
-                "sensor_size_w": 36,
-                "sensor_size_h": 24,
-                "sensor_px_w": 6720,
-                "sensor_px_h": 4480,
-            },
-        ]
-
-        self.controller.cursor.execute.side_effect = (
-            lambda *args: None
-        )  # Clear any existing side_effect or return_value
-        self.controller.cursor.fetchall.side_effect = fetchall_side_effect
-
-        # Act
+        # Call the method under test
         result = self.controller.get_by_field("brand", "Canon")
 
-        # Assert
-        self.assertEqual(len(result), 1)  # We expect one camera with the brand "Canon"
-        self.assertEqual(
-            result[0].brand, "Canon"
-        )  # Assuming Camera objects have a 'brand' attribute
-        self.assertEqual(
-            result[0].model, "EOS R6"
-        )  # Assuming Camera objects have a 'model' attribute
+        # Assert we got the expected camera
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].brand, "Canon")
+        self.assertEqual(result[0].model, "EOS R6")
 
     def test_get_by_field_like(self):
-        self.fail()
+        # Call the method under test
+        result = self.controller.get_by_field_like("brand", "Can")
+
+        # Assert we got the expected camera
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].brand, "Canon")
+        self.assertEqual(result[0].model, "EOS R6")
 
     def test_get_by_fields_like_and(self):
-        self.fail()
+        # Call the method under test
+        result = self.controller.get_by_fields_like_and(
+            {"brand": "Can", "model": "EOS"}
+        )
+
+        # Assert we got the expected camera
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].brand, "Canon")
+        self.assertEqual(result[0].model, "EOS R6")
 
     def test_get_by_fields_like_or(self):
-        self.fail()
+        # Call the method under test
+        result = self.controller.get_by_fields_like_or(
+            {"brand": "Can", "model": "D750"}
+        )
+
+        # Assert we got the expected camera
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].brand, "Canon")
+        self.assertEqual(result[0].model, "D750")
 
     def test_get_by_fields_with_operators(self):
         self.fail()
